6f5d6ddf034d8fccec36d9e2bcce57c51f414b44
Mino (Hung) <hungpikatv@gmail.com>
2026-03-20 13:27:28 +0700

    update auto-migrate guide

--- auto-migrate.bat
+++ auto-migrate.bat
@@ -1,3 +1,6 @@
 dotnet build
 dotnet ef migrations add MoveInstructorToClass --startup-project ../ELearning.API
 dotnet ef database update --startup-project ../ELearning.API
+
+# Đứng ở thư mục backend/ gõ:
+# dotnet ef migrations add MoveInstructorToClass --project ELearning.Infrastructure --startup-project ELearning.API
