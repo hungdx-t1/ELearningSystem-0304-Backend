85fcccb3093d034e909b589da481a9ef81ef725f
Mino (Hung) <hungpikatv@gmail.com>
2026-04-07 15:20:08 +0700

    added AddCreatorToCourse migration

---
+++ ELearning.Infrastructure/Migrations/20260407081732_AddCreatorToCourse.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ELearning.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatorToCourse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatorId",
+                table: "Courses",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_CreatorId",
+                table: "Courses",
+                column: "CreatorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Users_CreatorId",
+                table: "Courses",
+                column: "CreatorId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Users_CreatorId",
+                table: "Courses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_CreatorId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "CreatorId",
+                table: "Courses");
+        }
+    }
+}
