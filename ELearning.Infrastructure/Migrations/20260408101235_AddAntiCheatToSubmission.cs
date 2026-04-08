eb99b439c719bbc688e5f0496cd17ccd68a909c3
Mino (Hung) <hungpikatv@gmail.com>
2026-04-08 17:14:03 +0700

    migration: add anticheat and answer log in submission

---
+++ ELearning.Infrastructure/Migrations/20260408101235_AddAntiCheatToSubmission.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ELearning.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAntiCheatToSubmission : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CheatWarnings",
+                table: "Submissions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSubmitted",
+                table: "Submissions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "QuizAnswersJson",
+                table: "Submissions",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheatWarnings",
+                table: "Submissions");
+
+            migrationBuilder.DropColumn(
+                name: "IsSubmitted",
+                table: "Submissions");
+
+            migrationBuilder.DropColumn(
+                name: "QuizAnswersJson",
+                table: "Submissions");
+        }
+    }
+}
