13602e0a31653363adf549a2c1d25ac875cf6a4a
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2026-05-27 10:32:16 +0700

    added password validation

--- ELearning.Core/DTOs/Auth/AuthDtos.cs
+++ ELearning.Core/DTOs/Auth/AuthDtos.cs
@@ -1,5 +1,7 @@
 using ELearning.Core.DTOs.User;
 using ELearning.Core.Enums;
+using System.ComponentModel.DataAnnotations;
+using ELearning.Core.Common.Constants;
 
 namespace ELearning.Core.DTOs.Auth;
 
@@ -10,11 +12,25 @@ public record LoginResponseDto(string Token, UserResponseDto User);
 
 // Dữ liệu người dùng gửi lên để tạo tài khoản
 public record RegisterRequestDto(
-    string Email, 
-    string Password, 
-    string FullName, 
+    [Required][EmailAddress] string Email,
+    
+    [Required(ErrorMessage = "Vui lòng nhập mật khẩu.")]
+    [MinLength(8, ErrorMessage = "Mật khẩu phải từ 8 ký tự trở lên.")]
+    [RegularExpression(ValidationConstants.PasswordRegexPattern, ErrorMessage = ValidationConstants.PasswordErrorMessage)]
+    string Password,
+    
+    [Required] string FullName,
     UserRole Role = UserRole.Student // Sửa string thành UserRole
 );
 
 public record RequestChangePasswordDto(string OldPassword);
-public record ConfirmChangePasswordDto(string NewPassword, string OtpCode);
+
+
+public record ConfirmChangePasswordDto(
+    [Required(ErrorMessage = "Vui lòng nhập mật khẩu mới.")]
+    [MinLength(8, ErrorMessage = "Mật khẩu phải từ 8 ký tự trở lên.")]
+    [RegularExpression(ValidationConstants.PasswordRegexPattern, ErrorMessage = ValidationConstants.PasswordErrorMessage)]
+    string NewPassword, 
+    
+    [Required] string OtpCode
+);
